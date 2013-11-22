d5938079df804e9923e0173fd98cfcd3fe17fb67
Jorman Bustos <tille100@gmail.com>
2013-11-21 22:10:23 -0500

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -9,10 +9,10 @@
   function calling($id, $json){
       
     if($id==0){ 
-        $params = json_decode(stripslashes($json));
-        $user = $params->{'user'};
-        $surn = $params->{'surname'};
-        return user_controller::greet( $user, $surn );
+      $params = json_decode(stripslashes($json));
+      $user = $params->{'user'};
+      $surn = $params->{'surname'};
+      return user_controller::greet( $user, $surn );
     }
     
     return "";
