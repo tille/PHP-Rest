48aa6509dd5ba900072db4440ebe1adbe71ca6f5
Jorman Bustos <tille100@gmail.com>
2013-11-21 22:18:31 -0500

    deleting views

--- app/views/user/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-  ob_start(); 
-  session_start();
-  $id = $_SESSION['id'];
-  $pwd = $_SESSION['password'];
-  session_destroy();
-  $success = header('Location: '."http://localhost:8888/ff/services.php?q=change_operator_state&params=".$id.",0");
-?>
